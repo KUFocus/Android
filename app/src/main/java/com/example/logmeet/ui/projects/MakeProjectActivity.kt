eb83ff948e6b7f002697cdcadde305f28234dfcd
chrin05 <rinring105@gmail.com>
2024-08-09 22:45:34 +0900

    feat : 새 프로젝트 추가 페이지

---
+++ app/src/main/java/com/example/logmeet/ui/projects/MakeProjectActivity.kt
@@ -0,0 +1,26 @@
+package com.example.logmeet.ui.projects
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.logmeet.R
+import com.example.logmeet.databinding.ActivityMakeProjectBinding
+
+class MakeProjectActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMakeProjectBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMakeProjectBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
