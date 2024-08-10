05e8c8a17af9596227e1637d0ec74fdf0ff1151e
chrin05 <rinring105@gmail.com>
2024-08-10 18:52:37 +0900

    feat : 날짜 클릭 시 일정칸에 반영

--- app/src/main/java/com/example/logmeet/ui/component/WeeklyCalendar.kt
+++ app/src/main/java/com/example/logmeet/ui/component/WeeklyCalendar.kt
@@ -4,24 +4,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.time.LocalDate
 
 @Composable
-fun WeeklyCalendar() {
+fun WeeklyCalendar(
+    selectedDate : (String) -> Unit
+) {
     Column {
         WeeklyTitle()
         Spacer(modifier = Modifier.height(16.dp))
         DayOfWeek()
         Spacer(modifier = Modifier.height(16.dp))
-        DateOfWeek(date = LocalDate.now())
+        DateOfWeek(
+            date = LocalDate.now()
+        ) { selectedDate(it) }
     }
-}
-
-@Preview
-@Composable
-fun PreviewWeeklyCalendar() {
-    WeeklyCalendar()
 }
--- app/src/main/java/com/example/logmeet/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/logmeet/ui/home/HomeFragment.kt
@@ -1,24 +1,22 @@
 package com.example.logmeet.ui.home
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.logmeet.R
+import androidx.fragment.app.Fragment
 import com.example.logmeet.databinding.FragmentHomeBinding
 import com.example.logmeet.ui.component.WeeklyCalendar
-import com.example.logmeet.ui.component.WeeklyTitle
 import com.example.logmeet.ui.projects.MakeProjectActivity
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+    private val today: LocalDate = LocalDate.now()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +25,11 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
         binding.compHomeCalendar.setContent {
-            WeeklyCalendar()
+            WeeklyCalendar( selectedDate = {
+                setScheduleDate(it)
+                //일정 불러오는 api 연결
+            })
         }
-
 
         //프로젝트가 아무것도 없을 때
         binding.clHomeAddProject.setOnClickListener {
@@ -38,5 +38,14 @@ class HomeFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setScheduleDate(dayOfMonth: String) {
+        val beforeFormat = LocalDate.of(today.year, today.month, dayOfMonth.toInt())
+        val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+        val formattedDate = beforeFormat.format(formatter)
+
+        binding.tvHomeDate.text = formattedDate
     }
 }
