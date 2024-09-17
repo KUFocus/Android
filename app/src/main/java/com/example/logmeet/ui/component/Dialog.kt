f4be35d5c1315354d8716503036a2ab54a5f50b2
chrin05 <rinring105@gmail.com>
2024-09-17 23:00:32 +0900

    feat: 회의록 생성 Dialog 구현

---
+++ app/src/main/java/com/example/logmeet/ui/component/Dialog.kt
@@ -0,0 +1,123 @@
+package com.example.logmeet.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.example.logmeet.R
+
+@Composable
+fun MakeMinutesDialog(
+    title: String,
+    onClickCancel: () -> Unit,
+    onClickConfirm: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = { onClickCancel() },
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+        )
+    ) {
+        Card(
+            shape = RoundedCornerShape(8.dp),
+        )
+        {
+            Column(
+                modifier = Modifier
+                    .width(300.dp)
+                    .wrapContentHeight()
+                    .background(
+                        color = Color.White,
+                    ),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .clickable { onClickCancel() }
+                            .size(18.dp),
+                        painter = painterResource(id = R.drawable.ic_close),
+                        contentDescription = "닫기버튼"
+                    )
+                }
+                Text(
+                    text = title,
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        lineHeight = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                        fontWeight = FontWeight(700),
+                        color = Color(0xFF000000),
+                    )
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row(
+                    modifier = Modifier.padding(bottom = 10.dp)
+                ) {
+                    MakeMinutesBtn(image = R.drawable.ic_photo, content = "사진 업로드")
+                    Spacer(modifier = Modifier.width(18.dp))
+                    MakeMinutesBtn(image = R.drawable.ic_camera, content = "사진 촬영")
+                }
+                Row(
+                    modifier = Modifier.padding(bottom = 10.dp)
+                ) {
+                    MakeMinutesBtn(image = R.drawable.ic_video, content = "녹음 업로드")
+                    Spacer(modifier = Modifier.width(18.dp))
+                    MakeMinutesBtn(image = R.drawable.ic_microphone, content = "음성녹음")
+                }
+                MakeMinutesBtn(image = R.drawable.ic_text, content = "직접 작성")
+
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    MakeMinutesDialog(title = "회의록 생성 방식을 선택해주세요", onClickCancel = { /*TODO*/ }) {
+        
+    }
+    
+}
