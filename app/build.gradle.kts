d288b35e750902f14db923db400add48cfca02f8
chrin05 <rinring105@gmail.com>
2024-10-07 00:19:34 +0900

    feat : 메일전송 관련 dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.android.mail)
+    implementation(libs.android.activation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
