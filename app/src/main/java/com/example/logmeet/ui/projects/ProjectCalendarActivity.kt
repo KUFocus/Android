df98a16041fe73190cb692d89b0aa0b691e9fc27
chrin05 <rinring105@gmail.com>
2024-10-15 21:21:15 +0900

    feat : 프로젝트 캘린더

---
+++ app/src/main/java/com/example/logmeet/ui/projects/ProjectCalendarActivity.kt
@@ -0,0 +1,29 @@
+package com.example.logmeet.ui.projects
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.logmeet.R
+import com.example.logmeet.databinding.ActivityProjectCalendarBinding
+
+class ProjectCalendarActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProjectCalendarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityProjectCalendarBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
