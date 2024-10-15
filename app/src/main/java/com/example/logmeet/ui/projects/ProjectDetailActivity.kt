67d572e29fde0422fe496fa08c7ceac1073f6682
chrin05 <rinring105@gmail.com>
2024-10-15 19:57:37 +0900

    feat : 필요없는 import 삭제

--- app/src/main/java/com/example/logmeet/ui/projects/ProjectDetailActivity.kt
+++ app/src/main/java/com/example/logmeet/ui/projects/ProjectDetailActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
@@ -16,20 +15,15 @@ import com.example.logmeet.domain.entity.ProjectDrawableResources
 import com.example.logmeet.R
 import com.example.logmeet.data.dto.project.UserProjectDto
 import com.example.logmeet.data.dto.project.api_response.BaseResponseProjectInfoResult
-import com.example.logmeet.domain.entity.PeopleData
 import com.example.logmeet.databinding.ActivityProjectDetailBinding
-import com.example.logmeet.domain.entity.ProjectColorResources
 import com.example.logmeet.network.RetrofitClient
 import com.example.logmeet.tag
 import com.example.logmeet.ui.application.LogmeetApplication
-import com.example.logmeet.ui.showMinutesToast
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class ProjectDetailActivity : AppCompatActivity() {
     private lateinit var binding : ActivityProjectDetailBinding
