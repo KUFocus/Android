8ec0bb65101e40b7eaa4baab88fe547124b52bd8
chrin05 <rinring105@gmail.com>
2024-10-15 21:37:04 +0900

    feat : 프로젝트 스케줄 기능 연결

--- app/src/main/java/com/example/logmeet/ui/projects/ProjectCalendarActivity.kt
+++ app/src/main/java/com/example/logmeet/ui/projects/ProjectCalendarActivity.kt
@@ -1,15 +1,24 @@
 package com.example.logmeet.ui.projects
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide.init
 import com.example.logmeet.R
 import com.example.logmeet.databinding.ActivityProjectCalendarBinding
+import com.example.logmeet.domain.entity.ScheduleData
+import com.example.logmeet.ui.component.MonthlyCalendar
+import com.example.logmeet.ui.home.HomeScheduleAdapter
+import formatDate
 
 class ProjectCalendarActivity : AppCompatActivity() {
     lateinit var binding: ActivityProjectCalendarBinding
+    private lateinit var scheduleAdapter: HomeScheduleAdapter
+    private var scheduleList: ArrayList<ScheduleData> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityProjectCalendarBinding.inflate(layoutInflater)
@@ -24,6 +33,30 @@ class ProjectCalendarActivity : AppCompatActivity() {
             insets
         }
 
+        binding.compProjectCalendarCalendar.setContent {
+            MonthlyCalendar {
+                binding.tvProjectCalendarDate.text = formatDate(it.toString())
+            }
+        }
 
+        init()
+    }
+
+    private fun init() {
+        setProjectScheduleListData()
+        val isScheduleEmpty = scheduleList.isEmpty()
+        binding.clProjectCalendarNonschedule.visibility = if (isScheduleEmpty) View.VISIBLE else View.GONE
+        binding.rvProjectCalendarScheduleList.visibility = if (isScheduleEmpty) View.GONE else View.VISIBLE
+        if (!isScheduleEmpty) setScheduleRV()
+    }
+
+    private fun setProjectScheduleListData() {
+
+    }
+
+    private fun setScheduleRV() {
+        scheduleAdapter = HomeScheduleAdapter(scheduleList)
+        binding.rvProjectCalendarScheduleList.adapter = scheduleAdapter
+        binding.rvProjectCalendarScheduleList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 }
