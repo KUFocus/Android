c3bf845003ca7662c3a0cfc17c9d904a64199b4d
chrin05 <rinring105@gmail.com>
2024-10-16 17:45:39 +0900

    feat : 일자 형식 변환 함수 추가

--- app/src/main/java/com/example/logmeet/DateFunctions.kt
+++ app/src/main/java/com/example/logmeet/DateFunctions.kt
@@ -1,6 +1,8 @@
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
+import java.util.Locale
 
 fun formatDate(dayOfMonth: String): String? {
     return try {
@@ -16,6 +18,19 @@ fun formatDate(dayOfMonth: String): String? {
     } catch (e: DateTimeParseException) {
         e.printStackTrace()
         null
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
+
+fun reformatDate(dayOfMonth: String): String? {
+    return try {
+        val inputFormat = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault())
+        val outputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+        val date = inputFormat.parse(dayOfMonth)
+        outputFormat.format(date!!)
     } catch (e: Exception) {
         e.printStackTrace()
         null
