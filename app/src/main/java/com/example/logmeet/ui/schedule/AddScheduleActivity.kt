44382d0c0f293a82ec8bb1d46e6bae5d1b965bc3
chrin05 <rinring105@gmail.com>
2024-10-17 00:17:51 +0900

    feat : 일정 추가화면 추가

---
+++ app/src/main/java/com/example/logmeet/ui/schedule/AddScheduleActivity.kt
@@ -0,0 +1,26 @@
+package com.example.logmeet.ui.schedule
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.logmeet.R
+import com.example.logmeet.databinding.ActivityAddScheduleBinding
+
+class AddScheduleActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAddScheduleBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityAddScheduleBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
