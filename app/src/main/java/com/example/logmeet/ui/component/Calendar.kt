c56408b91046e0e64a57758df67df9611bbf7a22
chrin05 <rinring105@gmail.com>
2024-10-17 21:10:55 +0900

    feat: 월별 일정 조회 적용중

--- app/src/main/java/com/example/logmeet/ui/component/Calendar.kt
+++ app/src/main/java/com/example/logmeet/ui/component/Calendar.kt
@@ -1,8 +1,6 @@
 package com.example.logmeet.ui.component
 
 import DateOfMonth
-import android.app.Activity
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -13,9 +11,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.logmeet.R
-import com.example.logmeet.ui.home.HomeFragment
-import com.example.logmeet.ui.showMinutesToast
 import java.time.LocalDate
 
 @Composable
@@ -44,6 +39,11 @@ fun MonthlyCalendar(
     onAddScheduleComplete: (Int) -> Unit
 ) {
     var currentDate by remember { mutableStateOf<LocalDate>(LocalDate.now()) }
+    var monthlySchedules by remember { mutableStateOf<Map<LocalDate, List<String>>>(emptyMap()) }
+
+//    LaunchedEffect(currentDate.year, currentDate.month) {
+//        monthlySchedules = fetchMonthlySchedule(currentDate.year, currentDate.month.value)
+//    }
     Column {
         MonthlyTitle(
             isBottomSheet = isBottomSheet,
@@ -68,3 +68,8 @@ fun MonthlyCalendar(
         }
     }
 }
+
+//suspend fun fetchMonthlySchedule(year: Int, month: Int): Map<LocalDate, List<String>> {
+//    //월별 api 연결
+//    return
+//}
